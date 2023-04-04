e4854c8d08898a23bad528becd98dccd9eef799c
xyiorgyx <112989524+xyiorgyx@users.noreply.github.com>
2023-04-04 15:27:57 -0400

    removed doc
    One file was removed and another altered

--- Develop/client/src/utils/mutations.js
+++ Develop/client/src/utils/mutations.js
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const LOGINB_USER = gql`
+export const LOGIN_USER = gql`
     mutation loginUser($email: String!, $password: String!) {
         loginUser(email: $email, password: $password) {
             token
